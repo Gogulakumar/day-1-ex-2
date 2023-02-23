646257caa2cad47c4ed5868ad237e858c0c45cb2
Gogulakumar <gogulakumar2401@gmail.com>
2023-02-23 11:46:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/Day1Ex12Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1Ex12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1Ex12Application.class, args);
+	}
+
+}
